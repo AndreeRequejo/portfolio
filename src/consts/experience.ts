b31e688a3b6aa57bd6821777a2da8e8af6b2acbe
Andree <andree09requejo@gmail.com>
2026-03-10 18:11:58 -0500

    feat: agregar lista de experiencias

---
+++ src/consts/experience.ts
@@ -0,0 +1,29 @@
+export const experiences = [
+  {
+    role: "Practicante Preprofesional en Desarrollo de Sistemas",
+    company: "Universidad Nacional Pedro Ruiz Gallo",
+    period: "Dic. 2024 — Mar. 2025",
+    desc: [
+      "Participación en el desarrollo de un sistema web full stack con Laravel y React (Inertia.js)",
+      "Diseño e implementación de APIs REST con autenticación JWT, middlewares de seguridad y control de sesiones",
+      "Desarrollo de interfaces web responsivas orientadas a la gestión de asistencia docente",
+      "Implementación de control de roles y permisos para garantizar la seguridad y segregación de funciones entre usuarios",
+      "Automatización de la integración de datos entre dispositivos biométricos y el sistema web mediante scripts en Python, reduciendo la carga de procesos manuales",
+      "Implementación de procedimientos almacenados y triggers en MySQL para sincronizar y validar la información entre bases de datos",
+      "Uso de Git y GitHub para control de versiones y trabajo colaborativo",
+    ],
+    tags: ["Laravel", "React", "MySQL", "Inertia", "Python"],
+  },
+  {
+    role: "Full-Stack Developer",
+    company: "Textiles Creando Moda S.A.C",
+    period: "Ago. 2024 — Ene. 2025",
+    desc: [
+      "Diseñé la arquitectura general del sistema y la estructura de sus componentes visuales, asegurando una experiencia de usuario clara e intuitiva.",
+      "Desarrollo de funcionalidades CRUD y APIs REST utilizando Node.js y Express, orientadas a la gestión de ventas, productos e inventarios.",
+      "Implementación de mecanismos de autenticación y autorización con JWT, garantizando la seguridad y el acceso controlado a la información.",
+      "Realización de pruebas funcionales para integración backend-frontend, asegurando la correcta interacción entre ambos y la estabilidad del sistema.",
+    ],
+    tags: ["React", "ExpressJS", "Docker", "MySQL"],
+  },
+];

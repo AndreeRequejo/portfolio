4bc0ecc1b3568c33032767372bdaad5ba290c1dd
Andree <andree09requejo@gmail.com>
2026-03-10 19:16:37 -0500

    feat: agregar constantes de descripción y frase en un nuevo archivo para mejorar la gestión de contenido en AboutSection

---
+++ src/consts/about.ts
@@ -0,0 +1,18 @@
+export const description = `
+        Estudiante de Ingeniería de Sistemas y Computación con experiencia en
+        desarrollo full stack y partici­pación en proyectos de sistemas web.
+        Busco prácticas profesionales en desarrollo y mantenimiento de
+        aplicaciones informáticas, donde pueda aplicar mis conocimientos en
+        programación con Java, Python, así como en el desarrollo frontend con
+        HTML, CSS y frameworks modernos. Cuento con experiencia en análisis y
+        diseño de soluciones web, implementación de APIs REST, pruebas
+        funcionales, documentación técnica y soporte básico a usuarios. Poseo
+        competencias en análisis de datos, generación de reportes técnicos y
+        visualización de información, lo que me permite contribuir en
+        actividades de soporte técnico, documentación de procesos y análisis de
+        información para la toma de decisiones. Me caracterizo por mi
+        aprendizaje rápido, responsabilidad y actitud proactiva, con facilidad
+        para adaptarme a nuevos entornos tecnológicos.
+`;
+
+export const phrase = `"El mejor código es el que no necesita explicación, pero siempre la tiene."`
